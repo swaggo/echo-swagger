aed4c942c47acecba9c8a4de36251e34183608b1
webee.yw <webee.yw@gmail.com>
2019-07-09 19:16:57 +0800

    update

--- swagger.go
+++ swagger.go
@@ -2,72 +2,75 @@ package echoSwagger
 
 import (
 	"html/template"
-	"net/http"
-	"regexp"
 
 	"github.com/labstack/echo/v4"
-	"github.com/swaggo/files"
+	swaggerFiles "github.com/swaggo/files"
 	"github.com/swaggo/swag"
 )
 
 // Config stores echoSwagger configuration variables.
 type Config struct {
 	//The url pointing to API definition (normally swagger.json or swagger.yaml). Default is `doc.json`.
-	URL string
+	url string
+	// Prefix is the URL path prefix to strip from WebDAV resource paths.
+	prefix string
+	// //The path pointing to API definition file(normally swagger.json or swagger.yaml).
+	// docPath string
+	// // The server host
+	// host string
 }
 
-// URL presents the url pointing to API definition (normally swagger.json or swagger.yaml).
-func URL(url string) func(c *Config) {
-	return func(c *Config) {
-		c.URL = url
+// NewConfig create a default config
+func NewConfig() *Config {
+	return &Config{
+		url:    "doc.json",
+		prefix: "/",
 	}
 }
 
-// WrapHandler wraps swaggerFiles.Handler and returns echo.HandlerFunc
-var WrapHandler = EchoWrapHandler()
+// WithURL set the URL
+func (c *Config) WithURL(url string) *Config {
+	c.url = url
+	return c
+}
+
+//URL pointing to API definition (normally swagger.json or swagger.yaml). Default is `doc.json`.
+func (c *Config) URL() string {
+	return c.prefix + c.url
+}
+
+// RegisterEchoHandler for prefix
+func RegisterEchoHandler(method func(path string, h echo.HandlerFunc, m ...echo.MiddlewareFunc) *echo.Route, prefix string, config *Config, m ...echo.MiddlewareFunc) {
+	if config == nil {
+		config = NewConfig()
+	}
+	config.prefix = prefix
+
+	method(prefix+"*", EchoWrapHandler(config), m...)
+}
 
 // EchoWrapHandler wraps `http.Handler` into `echo.HandlerFunc`.
-func EchoWrapHandler(confs ...func(c *Config)) echo.HandlerFunc {
+func EchoWrapHandler(config *Config) echo.HandlerFunc {
+	if config == nil {
+		config = NewConfig()
+	}
 
 	handler := swaggerFiles.Handler
-
-	config := &Config{
-		URL: "doc.json",
-	}
-
-	for _, c := range confs {
-		c(config)
-	}
+	handler.Prefix = config.prefix
 
 	// create a template with name
 	t := template.New("swagger_index.html")
 	index, _ := t.Parse(indexTempl)
 
-	type pro struct {
-		Host string
-	}
-
-	var re = regexp.MustCompile(`(.*)(/|index\.html|doc\.json|favicon-16x16\.png|favicon-32x32\.png|/oauth2-redirect\.html|swagger-ui\.css|swagger-ui\.css\.map|swagger-ui\.js|swagger-ui\.js\.map|swagger-ui-bundle\.js|swagger-ui-bundle\.js\.map|swagger-ui-standalone-preset\.js|swagger-ui-standalone-preset\.js\.map)[\?|.]*`)
-
 	return func(c echo.Context) error {
-		var matches []string
-		if matches = re.FindStringSubmatch(c.Request().RequestURI); len(matches) != 3 {
-
-			return c.String(http.StatusNotFound, "404 page not found")
-		}
-		path := matches[2]
-		prefix := matches[1]
-		handler.Prefix = prefix
-
-		switch path {
-		case "/", "index.html":
+		switch c.Request().URL.Path {
+		case config.prefix, config.prefix + "index.html":
 			index.Execute(c.Response().Writer, config)
-		case "doc.json":
+		case config.prefix + "doc.json":
 			doc, _ := swag.ReadDoc()
 			c.Response().Write([]byte(doc))
 		default:
 			handler.ServeHTTP(c.Response().Writer, c.Request())
-
 		}
 
 		return nil
